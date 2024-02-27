dcd040627f707becd7f931f8fdc63e67c4931f77
Spycisann <sandyalfaizal49@gmail.com>
2024-02-27 15:51:05 +0700

    first commit

---
+++ pw2024_233040106/kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,14 @@
+<?php
+echo "Hello World!";
+echo "<br>";
+echo "kuliah pemrograman web";
+echo "<br>";
+echo "123 + 10";
+echo "<br>";
+echo 3.14;
+echo "<br>";
+echo false;
+echo "<br>";
+echo "jum'at";
+echo "<br>";
+echo "Sandy";
---
+++ pw2024_233040106/kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,5 @@
+<?php
+$nama = "sandy";
+$umur = 19;
+$kampus = "Universitan Pasundan";
+echo "<p>Halo, nama saya $nama saya $umur tahun, kuliah di $kampus</p>";
